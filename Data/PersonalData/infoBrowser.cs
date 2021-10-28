fb796379d811ca623f9fc08f56f324b796882651
Alexander Miranda Osorio <amosorio@veritran.com>
2021-10-28 15:00:10 -0500

    Autenticacion Personalizada - Alex

---
+++ Data/PersonalData/infoBrowser.cs
@@ -0,0 +1,45 @@
+﻿using portal_pets.Data.Request;
+using portal_pets.Data.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portal_pets.Data.Personal
+{
+    public class infoBrowser
+    {
+        public LoginRequest user { get; set; }
+        public List<infoRoles> roles { get; set; }
+        public List<infoMenu> menus { get; set; }
+    }
+
+    public class infoDataMenu
+    {
+        public List<infoMenu> menus { get; set; }
+    }
+
+    public class infoMenu
+    {
+        public String type { get; set; }
+        public String url { get; set; }
+        public String icono { get; set; }
+        public String code { get; set; }
+        public String option { get; set; }
+        public String parent { get; set; }
+        public Int32 state { get; set; }
+        public String id { get; set; }
+        public String idparent { get; set; }
+        public String permission { get; set; }
+    }
+
+
+    public class infoRoles
+    {
+        public String user { get; set; }
+        public String rol { get; set; }
+
+    }
+
+
+}
---
+++ Data/Request/AuthRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portal_pets.Data.Request
+{
+    public class AuthRequest
+    {
+        public LoginRequest entity { get; set; }
+        public Status status { get; set; }
+    }
+
+    public class LoginRequest 
+    {
+        public String token { get; set; }
+        public DateTime expiration { get; set; }
+        public String typedoc { get; set; }
+        public String namedoc { get; set; }
+        public String iduser { get; set; }
+        public String idcliente { get; set; }
+        public String user { get; set; }
+        public String name { get; set; }
+        public String lastname { get; set; }
+        public String numdoc { get; set; }
+        public String phone { get; set; }
+        public String email { get; set; }
+        public String address { get; set; }
+
+        [NotMapped]
+        public virtual String nombrefull { get { return (name == null ? "" : name.Trim().ToUpper()) + " " + (lastname ==null ? "" : lastname.Trim().ToUpper()); } }
+    }
+
+}
