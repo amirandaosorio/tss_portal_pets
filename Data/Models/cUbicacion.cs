b48ce4a610ebe5d5dff5b2a67d3fd70224ee1432
Alexander Miranda Osorio <amosorio@veritran.com>
2021-11-06 19:33:41 -0500

    Actualizar

--- Data/Models/cUbicacion.cs
+++ Data/Models/cUbicacion.cs
@@ -24,18 +24,18 @@ namespace portal_pets.Data.Models
             new cUbicacion() { codigo = "170", nombre = "COLOMBIA", padre="0", tipo=1 },
             new cUbicacion() { codigo = "862", nombre = "VENEZUELA", padre="0", tipo=1 },
             new cUbicacion() { codigo = "862001", nombre = "DISTRITO CAPITAL", padre="862", tipo=2 },
-            new cUbicacion() { codigo = "862001001", nombre = "CARACAS", padre="001", tipo=3 },
+            new cUbicacion() { codigo = "862001001", nombre = "CARACAS", padre="862001", tipo=3 },
 
             new cUbicacion() { codigo = "17066", nombre = "RISARALDA", padre="170", tipo=2 },
             new cUbicacion() { codigo = "17076", nombre = "VALLE DEL CAUCA", padre="170", tipo=2 },
             new cUbicacion() { codigo = "17063", nombre = "QUINDIO", padre="170", tipo=2 },
-            new cUbicacion() { codigo = "17066001", nombre = "PEREIRA", padre="66", tipo=3 },
-            new cUbicacion() { codigo = "17066170", nombre = "DOSQUEBRADAS", padre="66", tipo=3 },
-            new cUbicacion() { codigo = "17066682", nombre = "SANTA ROSA DE CABAL", padre="66", tipo=3 },
-            new cUbicacion() { codigo = "17066400", nombre = "LA VIRGINIA", padre="66", tipo=3 },
-            new cUbicacion() { codigo = "17076147", nombre = "CARTAGO", padre="76", tipo=3 },
-            new cUbicacion() { codigo = "17063001", nombre = "ARMENIA", padre="63", tipo=3 },
-            new cUbicacion() { codigo = "17066440", nombre = "MARSELLA", padre="66", tipo=3 }
+            new cUbicacion() { codigo = "17066001", nombre = "PEREIRA", padre="17066", tipo=3 },
+            new cUbicacion() { codigo = "17066170", nombre = "DOSQUEBRADAS", padre="17066", tipo=3 },
+            new cUbicacion() { codigo = "17066682", nombre = "SANTA ROSA DE CABAL", padre="17066", tipo=3 },
+            new cUbicacion() { codigo = "17066400", nombre = "LA VIRGINIA", padre="17066", tipo=3 },
+            new cUbicacion() { codigo = "17076147", nombre = "CARTAGO", padre="17076", tipo=3 },
+            new cUbicacion() { codigo = "17063001", nombre = "ARMENIA", padre="17063", tipo=3 },
+            new cUbicacion() { codigo = "17066440", nombre = "MARSELLA", padre="17066", tipo=3 }
             };
         }
 
